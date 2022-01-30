dacc92ad45216051a93f4054854211e95ad640b7
andy-devs <markushin.andrew@gmail.com>
2022-01-30 14:48:33 +0600

    start

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+	return (
+		<>
+			<nav>
+				<h1>Yo</h1>
+			</nav>
+		</>
+	);
+};
+
+export default Navbar;
