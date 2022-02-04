f77015a91db2cf447cef4e32a9bfbc922c51fe5b
andy-devs <markushin.andrew@gmail.com>
2022-02-04 10:28:21 +0600

    Jobs Section

--- src/components/Home/HomeJobs.js
+++ src/components/Home/HomeJobs.js
@@ -2,6 +2,12 @@ import styles from './HomeJobs.module.css';
 import JobCard from '../UI/JobCard';
 import targetMarketing from '../../assets/images/target-marketing.jpg';
 import targetMarketingIcon from '../../assets/images/target-marketing-icon.svg';
+import contextMarketing from '../../assets/images/context-marketing.jpg';
+import contextMarketingIcon from '../../assets/images/context-marketing-icon.svg';
+import smm from '../../assets/images/smm.jpg';
+import smmIcon from '../../assets/images/smm-icon.svg';
+import developmentDesign from '../../assets/images/development-design.jpg';
+import developmentDesignIcon from '../../assets/images/development-design-icon.svg';
 
 const HomeJobs = () => {
 	const jobs = [
@@ -9,6 +15,30 @@ const HomeJobs = () => {
 			image: targetMarketing,
 			title: 'Таргетированная реклама',
 			icon: targetMarketingIcon,
+			description:
+				'более 50 проверенных исполнителей в нише бьюти, строительстве и...',
+			buttonText: 'Искать',
+		},
+		{
+			image: contextMarketing,
+			title: 'Контекстная реклама',
+			icon: contextMarketingIcon,
+			description:
+				'более 50 проверенных исполнителей в нише бьюти, строительстве и...',
+			buttonText: 'Искать',
+		},
+		{
+			image: smm,
+			title: 'SMM-продвижение',
+			icon: smmIcon,
+			description:
+				'более 50 проверенных исполнителей в нише бьюти, строительстве и...',
+			buttonText: 'Искать',
+		},
+		{
+			image: developmentDesign,
+			title: 'Разработка сайтов и дизайн',
+			icon: developmentDesignIcon,
 			description:
 				'более 50 проверенных исполнителей в нише бьюти, строительстве и...',
 			buttonText: 'Искать',
--- src/components/UI/JobCard.js
+++ src/components/UI/JobCard.js
@@ -6,9 +6,11 @@ const JobCard = ({ image, icon, title, description, buttonText }) => {
 		<div className={styles['job-card']}>
 			<img className={styles['job-card__image']} src={image} alt={title} />
 			<h3 className={styles['job-card__title']}>
-				<span className={styles['job-card__icon']}>
-					<img src={icon} alt={`icon for ${title}`} />
-				</span>
+				<img
+					className={styles['job-card__icon']}
+					src={icon}
+					alt={`icon for ${title}`}
+				/>
 				{title}
 			</h3>
 			<p className={styles['job-card__description']}>{description}</p>
