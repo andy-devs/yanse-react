dc180b017adf4472967801f375b8f48b3d9e82e5
andy-devs <markushin.andrew@gmail.com>
2022-02-04 10:52:39 +0600

    light grey to white fix

--- src/components/Home/HomeReg.js
+++ src/components/Home/HomeReg.js
@@ -1,5 +1,8 @@
 import styles from './HomeReg.module.css';
 import Button from '../UI/Button';
+import greenGuy from '../../assets/images/green-guy-icon.svg';
+import yellowGuy from '../../assets/images/yellow-guy-icon.svg';
+import purpleGuy from '../../assets/images/purple-guy-icon.svg';
 
 const HomeReg = () => {
 	return (
@@ -9,7 +12,29 @@ const HomeReg = () => {
 					Вы исполнитель и ищите клиентов? Находите проекты в нишах, с которыми
 					уже работали
 				</h3>
-				<div className={styles['home-reg__head-images']}></div>
+				<div className={styles['home-reg__head-images']}>
+					<img
+						className={
+							styles['home-reg__head-images__item'] + styles['first-image']
+						}
+						src={greenGuy}
+						alt='green guy icon'
+					/>
+					<img
+						className={
+							styles['home-reg__head-images__item'] + styles['second-image']
+						}
+						src={yellowGuy}
+						alt='yellow guy icon'
+					/>
+					<img
+						className={
+							styles['home-reg__head-images__item'] + styles['third-image']
+						}
+						src={purpleGuy}
+						alt='purple guy icon'
+					/>
+				</div>
 			</div>
 			<p className={styles['home-reg__description']}>
 				Хотите, чтобы не вы вечно искали проекты, а вас находили заказчики? И не
